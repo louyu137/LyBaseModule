b3817b1f065b09408a16e8ca15c9ba70685f5bdd
DESKTOP-C53ATOC\sdj003 <77053942@qq.com>
2018-07-31 08:45:11 +0800

    更新人：louyu

--- lylibrary/src/main/java/cn/louyu/lylibrary/core/annotation/Injector.java
+++ lylibrary/src/main/java/cn/louyu/lylibrary/core/annotation/Injector.java
@@ -1,7 +1,6 @@
 package cn.louyu.lylibrary.core.annotation;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.view.View;
 
 import java.lang.reflect.AccessibleObject;
@@ -11,32 +10,24 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Injector {
- 
-	private static List<? extends ProcessorIntf<? extends AccessibleObject>> chain
+
+    private static List<? extends ProcessorIntf<? extends AccessibleObject>> chain
             = Arrays.asList(new InjectViewProcessor(), new OnClickProcessor());
 
-	/**
+    /**
      * 初始化Activity
      * */
     public static void inject(Activity act) {
-    	//传入activity实例和rootview
+        //传入activity实例和rootview
         inject(act,act.getWindow().getDecorView());
     }
 
-    /**
-     * 初始化Fragment
-     * */
-    public static void inject(Fragment act) {
-        //传入activity实例和rootview
-        inject(act,act.getView().getRootView());
-    }
- 
     public static void inject(Object obj, View rootView) {
         final Class<?> aClass = obj.getClass();
         //获取当前活动中所有声明的属性，包括私有属性
         final Field[] declaredFields = aClass.getDeclaredFields();
         for (Field f : declaredFields) {
-           doChain(obj,f,rootView);
+            doChain(obj,f,rootView);
         }
         //获取当前活动中所有声明的方法，包括私有方法
         final Method[] declaredMethods = aClass.getDeclaredMethods();
@@ -46,11 +37,11 @@ public class Injector {
     }
     //把每个遍历到的方法或者属性，甚至是构造方法，类等等通过处理器链来询问这个注解你accept吗？接受则交给它来处理
     private static void doChain(Object obj,AccessibleObject ao, View rootView) {
-            for (ProcessorIntf p : chain) {
-            	//判断当前AccessibleObject(Field,Method都继承了此类)是否为ProcessorIntf具体子类类型的注解
-                if(p.accept(ao)) {
-                	p.process(obj,rootView,ao);
-                }
+        for (ProcessorIntf p : chain) {
+            //判断当前AccessibleObject(Field,Method都继承了此类)是否为ProcessorIntf具体子类类型的注解
+            if(p.accept(ao)) {
+                p.process(obj,rootView,ao);
             }
+        }
     }
 }
--- lylibrary/src/main/java/cn/louyu/lylibrary/core/utils/tools/Tree.java
+++ lylibrary/src/main/java/cn/louyu/lylibrary/core/utils/tools/Tree.java
@@ -10,9 +10,9 @@ public class Tree<T> {
     private T data; //携带的数据
     private List<Tree<T>> childs; //孩子节点
     private Tree<T> parent; //此节点的父节点
-    
+
     private Tree(T data){
-       this(data,null);
+        this(data,null);
     }
 
     public Tree(T data,Tree<T> parent){
@@ -41,7 +41,6 @@ public class Tree<T> {
      * 清空树
      * */
     public void clear(){
-        data=null;
         childs.clear();
     }
 
@@ -68,19 +67,19 @@ public class Tree<T> {
     private int dept(Tree<T> tree){
         if(!tree.hasChilds()) return 0;
         int [] array=new int[childs.size()];
-       for(int i=0;i<childs.size();i++){
-           array[i]=childs.get(i).dept(childs.get(i));
-       }
-       //升序排序
+        for(int i=0;i<childs.size();i++){
+            array[i]=childs.get(i).dept(childs.get(i));
+        }
+        //升序排序
         Arrays.sort(array);
-       return array[array.length-1]+1;
+        return array[array.length-1]+1;
     }
 
     /**
      * 这颗树的总节点数
      * */
     public int size(){
-       return size(this)+1;
+        return size(this)+1;
     }
 
     /**
@@ -153,7 +152,7 @@ public class Tree<T> {
      * （1）.访问根结点；
      * （2）.按照从左到右的次序行根遍历根结点的第一棵子树；
      * */
-    private List<Tree<T>> preVisit(Tree<T> tree){
+    public List<Tree<T>> preVisit(Tree<T> tree){
         List<Tree<T>> values=new ArrayList<Tree<T>>();
         if (tree.isEmpty()) return values;
         values.add(tree);
@@ -166,7 +165,7 @@ public class Tree<T> {
     /**
      * 前根遍历（深度遍历）
      * */
-    public List<T> preVisit(){
+    public List<T> preVisitValue(){
         List<Tree<T>> values=preVisit(this);
         List<T> vals=new ArrayList<T>();
         for (Tree<T> t : values) {
@@ -176,11 +175,18 @@ public class Tree<T> {
     }
 
     /**
+     * 前根遍历（深度遍历）
+     * */
+    public List<Tree<T>> preVisit(){
+        return preVisit(this);
+    }
+
+    /**
      * 后根遍历
      * （1）.按照从左到右的次序行根遍历根结点的第一棵子树；
      * （2）.访问根结点；
      * */
-    private List<Tree<T>> sufVisit(Tree<T> tree){
+    public List<Tree<T>> sufVisit(Tree<T> tree){
         List<Tree<T>> values=new ArrayList<Tree<T>>();
         if (tree.isEmpty()) return values;
         for(int i=0;i<childs.size();i++){
@@ -193,7 +199,7 @@ public class Tree<T> {
     /**
      * 后根遍历（深度遍历）
      * */
-    public List<T> sufVisit(){
+    public List<T> sufVisitValue(){
         List<Tree<T>> values=sufVisit(this);
         List<T> vals=new ArrayList<T>();
         for (Tree<T> t : values) {
@@ -203,13 +209,20 @@ public class Tree<T> {
     }
 
     /**
+     * 后根遍历（深度遍历）
+     * */
+    public List<Tree<T>> sufVisit(){
+        return sufVisit(this);
+    }
+
+    /**
      * 层次遍历（广度遍历）
      * (1) 先访问根节点
      * (2) 访问第n层时，将第n+1层的节点放入队列
      * (3) 进入下一层
      * (4) 重复上面的操作
      * */
-    private List<Tree<T>> levelVisit(Tree<T> tree){
+    public List<Tree<T>> levelVisit(Tree<T> tree){
         List<Tree<T>> values=new ArrayList<Tree<T>>();
         if(tree==null) return values;
         Queue<Tree<T>> queue=new LinkedList<Tree<T>>();
@@ -233,7 +246,7 @@ public class Tree<T> {
     /**
      * 层次遍历（广度遍历）
      * */
-    public List<T> levelVisit(){
+    public List<T> levelVisitValue(){
         List<Tree<T>> values=levelVisit(this);
         List<T> vals=new ArrayList<T>();
         for (Tree<T> t : values) {
@@ -243,10 +256,44 @@ public class Tree<T> {
     }
 
     /**
+     * 层次遍历（广度遍历）
+     * */
+    public List<Tree<T>> levelVisit(){
+        return levelVisit(this);
+    }
+
+    /**
      * 树中的节点是否包含某个值
      * */
     public boolean containsValue(T val){
-        List<T> values=preVisit();
+        List<T> values=preVisitValue();
         return values.contains(val);
     }
+
+    /**
+     * 获取当前的父类节点
+     * */
+    public Tree<T> getParent(){
+        return this.parent;
+    }
+
+    /**
+     * 判断是不是当前节点的祖先节点
+     * */
+    public boolean isAncestor(Tree<T> ancestor){
+        if (this.parent == null||ancestor==null) return false;
+        if (this.parent==ancestor) return true;
+        return this.parent.isAncestor(ancestor);
+    }
+
+    /**
+     * 获取节点所在的层级（相对于根,从0开始）
+     * */
+    public int selfTierNum(){
+        if(this.parent==null){
+            return 0;
+        }
+        return this.parent.selfTierNum()+1;
+    }
+
 }
