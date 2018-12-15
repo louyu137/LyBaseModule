4d233dcfade9c588bb18397dd13de036e666f169
DESKTOP-C53ATOC\sdj003 <77053942@qq.com>
2018-12-15 15:45:55 +0800

    louyu 2018-12-15 15:45:46

--- app/src/main/java/cn/louyu/lybasemodule/MainActivity.java
+++ app/src/main/java/cn/louyu/lybasemodule/MainActivity.java
@@ -1,63 +1,45 @@
 package cn.louyu.lybasemodule;
 
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v4.app.NotificationCompat;
 import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.ImageView;
-import android.widget.Switch;
 
-import java.net.URL;
+import com.alibaba.fastjson.JSON;
 
-import cn.louyu.lylibrary.core.annotation.InjectView;
-import cn.louyu.lylibrary.core.annotation.Injector;
-import cn.louyu.lylibrary.core.utils.picasso.PicassoHelper;
+import cn.louyu.lymvpframework.base.BaseActivity;
+import cn.louyu.lymvpframework.model.ResultMsg;
+import cn.louyu.lymvpframework.utils.lyokhttp.LyOkHttpClient;
+import cn.louyu.lymvpframework.utils.lyokhttp.handler.ObjectCallbackOnUIHandler;
 
 
-public class MainActivity extends BaseActionbarActivity{
-
-    @InjectView(R.id.cacheImage)
-    private ImageView cacheImage;
-
-    public static void showNotifictionIcon(Context context,String msg) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-//        Intent intent = new Intent(context, XXXActivity.class);//将要跳转的界面
-        Intent intent = new Intent();//只显示通知，无页面跳转
-        builder.setAutoCancel(true);//点击后消失
-        builder.setSmallIcon(R.drawable.ic_launcher_foreground);//设置通知栏消息标题的头像
-        builder.setDefaults(NotificationCompat.DEFAULT_SOUND);//设置通知铃声
-        builder.setTicker("接收到一条消息");
-        builder.setContentTitle("服务器推送");
-        builder.setContentText(msg);
-        //利用PendingIntent来包装我们的intent对象,使其延迟跳转
-        PendingIntent intentPend = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-        builder.setContentIntent(intentPend);
-        NotificationManager manager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
-        manager.notify(0, builder.build());
-    }
+public class MainActivity extends BaseActivity{
 
     @Override
     public int getLayoutId() {
         return R.layout.activity_main;
     }
 
-    @Override
-    public void initView() {
-        Injector.inject(context);
-    }
+    public void request(View view){
+        LyOkHttpClient client=new LyOkHttpClient();
+        ResultMsg msg=new ResultMsg();
+        msg.Msg="测试,字符串";
+        msg.Success=true;
+        msg.Data="LyOkhttp Test";
+        String json = JSON.toJSONString(msg);
+        client.sendJson("http://192.168.0.106:8080/api/testOk", json, new ObjectCallbackOnUIHandler<String>() {
+            @Override
+            public void onBeforeCallback() {
 
-    @Override
-    public void initData() {
-        PicassoHelper.loadImageFromUrl(context,Uri.parse("http://5b0988e595225.cdn.sohucs.com/images/20171110/575a5ed4311447ccb32fe664c5d26351.jpeg"),cacheImage);
-    }
+            }
 
-    @Override
-    public void initListener() {
+            @Override
+            public void onSuccessCallback(String data) {
+                showToast(data);
+            }
 
+            @Override
+            public void onFailureCallback(int code, String msg) {
+
+            }
+        });
     }
 }
--- lylibrary/module/zxing/src/main/java/com/google/zxing/client/android/IScanSuccess.java
+++
@@ -1,7 +0,0 @@
-package com.google.zxing.client.android;
-
-public interface IScanSuccess {
-  //TODO===扫码成功后回调=====
-  void onScanResult(String result);
-  //TODO======================
-}
